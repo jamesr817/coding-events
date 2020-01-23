3ba3594128e9a6a092b86163b77a9d09838e9920
James Rodriguez <jamesr817@gmail.com>
2020-01-23 15:19:59 -0600

    Got all the code typed up, however, getting errors when clicking on 'All Categories' and 'Create Category'. Need to look over and check that there are no typos or inconsistencies within code.

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,6 +3,10 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 public class EventCategory {
@@ -11,6 +15,9 @@ public class EventCategory {
     @GeneratedValue
     private int id;
 
+    @Size(min = 3, max = 50, message = "Category name must be between 3-50 characters.")
+    @NotBlank(message = "Name for Category required.")
+    @NotNull(message = "Cannot be null, please try again.")
     private String name;
 
     public EventCategory(String name){
@@ -32,4 +39,19 @@ public class EventCategory {
     }
 
     //TODO: Should there be custom @Override methods like the ones in the Event model class? Why or why not?
+    //The answer is yes
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventCategory that = (EventCategory) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
