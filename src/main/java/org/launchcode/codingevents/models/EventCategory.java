701964b2246922c1e9848144ab604769c76647a8
James Rodriguez <jamesr817@gmail.com>
2020-01-27 01:10:24 -0600

    Conituing our follwo-along with Chapter 18-Relationships in ORM, we picked up in 18.3.1 video 'Coding Events 5.3 One-to-Many' and aadded a one to many relationship between 'EventsCategory' and 'Event' model classes. We made some additions to the 'EventController' class and the eventsCategories template 'index.html' file to allow users to view a list of events for a given category. Look at Chapter 18 and notes in code for details on how  this was done.

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,9 +1,12 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
@@ -12,6 +15,17 @@ public class EventCategory extends AbstractEntity {
     @NotBlank(message = "Name for new category is required. Please try again.")
     @NotNull(message = "Field cannot be null.")
     private String name;
+
+    //Starting to set up our one to many relationship
+    /*We start by doing just what we did in the Event model class by adding an annotation. Since this is going to be a
+    one to many relationship though, we have to use @OneToMany Annotation. We also need to add a parameter to this
+    annotation that tells hibernate how to determine which events are in a given category. We do this with the
+    "mappedBy" keyword, and in this case we're mapping by eventCategory.- mappedBy = "eventCategory".
+    The mappedBy value has to be the name of the field in the class we're storing here that creates this relationship.
+    Which is located in the "Events" model class. This now applies a foreign key reference to an event category.
+     */
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(String name){
         this.name = name;
@@ -25,6 +39,11 @@ public class EventCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    //Our getter for our new field, no need to add a setter.
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
